87ee7e2d42624d4698142b7006da9f7f0b3e1a7c
tudo <dongocthanhtuwork@gmail.com>
2025-04-02 18:46:16 +0900

    mmc:pwrseq: sprd power on/off over sysfs

--- drivers/mmc/core/pwrseq_sd8787.c
+++ drivers/mmc/core/pwrseq_sd8787.c
@@ -24,22 +24,19 @@
 
 #include "pwrseq.h"
 
+#define WILC_SYSFS_POWERCTL 1
+
 struct mmc_pwrseq_sd8787 {
 	struct mmc_pwrseq pwrseq;
 	struct gpio_desc *reset_gpio;
 	struct gpio_desc *pwrdn_gpio;
     struct mutex gpio_lock; // Protect access to GPIOs
-	// FIXME: Add HH300 do not support suspend/resume, so add sysfs control to 
-	// turn on/off the power. But this driver is also used by SPRD so add a flag
-	// to dertermine who controls the power
 	bool use_sysfs_control;
 };
 
 static struct mmc_pwrseq_sd8787 *global_pwrseq; // For sysfs access
 
-
 #define to_pwrseq_sd8787(p) container_of(p, struct mmc_pwrseq_sd8787, pwrseq)
-
 
 // Sysfs attribute to manually control GPIOs
 static ssize_t power_ctl_store(struct device *dev, struct device_attribute *attr,
@@ -104,32 +101,41 @@ static void mmc_pwrseq_sd8787_power_off(struct mmc_host *host)
 	gpiod_set_value_cansleep(pwrseq->pwrdn_gpio, 0);
 	gpiod_set_value_cansleep(pwrseq->reset_gpio, 0);
 }
+
+// During mmc_rescan it power on/off card.
 static void mmc_pwrseq_wilc1000_pre_power_on(struct mmc_host *host)
 {
-	struct mmc_pwrseq_sd8787 *pwrseq = to_pwrseq_sd8787(host->pwrseq);
+	#if WILC_SYSFS_POWERCTL == 1
+		(void) host;
+	#else
+		struct mmc_pwrseq_sd8787 *pwrseq = to_pwrseq_sd8787(host->pwrseq);
+		/* The pwrdn_gpio is really CHIP_EN, reset_gpio is RESETN */
+		 if (!pwrseq->use_sysfs_control) { // Only control power if not using sysfs
+			mutex_lock(&pwrseq->gpio_lock);
+			gpiod_set_value_cansleep(pwrseq->pwrdn_gpio, 1);
+			msleep(5);
+			gpiod_set_value_cansleep(pwrseq->reset_gpio, 1);
+			mutex_unlock(&pwrseq->gpio_lock);
+		}
+	#endif
+}
 
-    	mutex_lock(&pwrseq->gpio_lock);
-	/* The pwrdn_gpio is really CHIP_EN, reset_gpio is RESETN */
-	 if (!pwrseq->use_sysfs_control) { // Only control power if not using sysfs
-		gpiod_set_value_cansleep(pwrseq->pwrdn_gpio, 1);
-		msleep(5);
-		gpiod_set_value_cansleep(pwrseq->reset_gpio, 1);
-	}
-	mutex_unlock(&pwrseq->gpio_lock);
-
-}\
 static void mmc_pwrseq_wilc1000_power_off(struct mmc_host *host)
 {
-	struct mmc_pwrseq_sd8787 *pwrseq = to_pwrseq_sd8787(host->pwrseq);
+	#if WILC_SYSFS_POWERCTL == 1
+		(void) host;
+	#else
+		struct mmc_pwrseq_sd8787 *pwrseq = to_pwrseq_sd8787(host->pwrseq);
 
-    mutex_lock(&pwrseq->gpio_lock);
-	if (!pwrseq->use_sysfs_control) { // Only control power if not using sysfs
-		gpiod_set_value_cansleep(pwrseq->reset_gpio, 0);
-		gpiod_set_value_cansleep(pwrseq->pwrdn_gpio, 0);
-	}
-	
-    mutex_unlock(&pwrseq->gpio_lock);
+		if (!pwrseq->use_sysfs_control) {
+			mutex_lock(&pwrseq->gpio_lock);
+			gpiod_set_value_cansleep(pwrseq->reset_gpio, 0);
+			gpiod_set_value_cansleep(pwrseq->pwrdn_gpio, 0);
+			mutex_unlock(&pwrseq->gpio_lock);
+		}
+	#endif
 }
+
 static const struct mmc_pwrseq_ops mmc_pwrseq_wilc1000_ops = {
 	.pre_power_on = mmc_pwrseq_wilc1000_pre_power_on,
 	.power_off = mmc_pwrseq_wilc1000_power_off,
@@ -156,6 +162,7 @@ static int mmc_pwrseq_sd8787_probe(struct platform_device *pdev)
 	pwrseq = devm_kzalloc(dev, sizeof(*pwrseq), GFP_KERNEL);
 	if (!pwrseq)
 		return -ENOMEM;
+	mutex_init(&pwrseq->gpio_lock);
 
 	match = of_match_node(mmc_pwrseq_sd8787_of_match, pdev->dev.of_node);
 
@@ -170,20 +177,21 @@ static int mmc_pwrseq_sd8787_probe(struct platform_device *pdev)
 	pwrseq->pwrseq.dev = dev;
 	pwrseq->pwrseq.ops = match->data;
 	pwrseq->pwrseq.owner = THIS_MODULE;
-	pwrseq->use_sysfs_control = false; // Default to MMC control
+	// Default control by sysfs attribute power_ctl
+	pwrseq->use_sysfs_control = true;
 
 	platform_set_drvdata(pdev, pwrseq);
 
 	// Register sysfs attribute
 	global_pwrseq = pwrseq; // Set global reference for sysfs
-	
+
 	ret = device_create_file(dev, &dev_attr_power_ctl);
 	if (ret) {
 		dev_err(dev, "sysfs attribute\n");
 		return ret;
 	}
 	dev_info(dev, "Sysfs attribute /sys/devices/platform/%s/power_ctl created\n", dev_name(&pdev->dev));
-	
+
 	return mmc_pwrseq_register(&pwrseq->pwrseq);
 }
 
